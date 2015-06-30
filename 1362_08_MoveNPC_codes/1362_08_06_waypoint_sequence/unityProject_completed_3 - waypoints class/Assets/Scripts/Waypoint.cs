0061dafd8eec5db9d37894d9ba3d3b5b1b0e52d0
Matt Smith <dr_matt_smith@me.com>
2015-06-30 21:41:25 +0100

    v2.6 - ch8 codes updated

---
+++ 1362_08_MoveNPC_codes/1362_08_06_waypoint_sequence/unityProject_completed_3 - waypoints class/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Waypoint: MonoBehaviour {
+	public Waypoint[] waypoints;
+	
+	public Waypoint GetNextWaypoint () {
+		return waypoints[ Random.Range(0, waypoints.Length) ];
+	}
+}

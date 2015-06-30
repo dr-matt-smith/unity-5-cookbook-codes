c9b4ed39369ccb481f9a9bf2cb6a88ebfd86b6f5
Matt Smith <dr_matt_smith@me.com>
2015-06-30 16:49:41 +0100

    v2.5 - ch8 flee-from recipe code updated

--- 1362_08_MoveNPC_codes/1362_08_05_NPCSeekOrFlee/unityProject_completed_3_fleeDestination/Assets/NavMesh - destination/Scripts/ArrowNPCMovement.cs
+++ 1362_08_MoveNPC_codes/1362_08_05_NPCSeekOrFlee/unityProject_completed_3_fleeDestination/Assets/NavMesh - destination/Scripts/ArrowNPCMovement.cs
@@ -3,41 +3,28 @@ using System.Collections;
 
 public class ArrowNPCMovement : MonoBehaviour {
 	public GameObject targetGO;
-	public bool fleeFromTarget = true;
 	private NavMeshAgent navMeshAgent;
-	private float runAwayDistance = 10;
+	private float runAwayMultiplier = 2;
+	private float runAwayDistance;
 	
-	void Start()
-	{
+	void Start(){
 		navMeshAgent = GetComponent<NavMeshAgent>();
+		runAwayDistance = navMeshAgent.stoppingDistance * runAwayMultiplier;
+	}
+	
+	void Update () {
+		Vector3 enemyPosition = targetGO.transform.position;
+		float distanceFromEnemy = Vector3.Distance(transform.position, enemyPosition);
+		if (distanceFromEnemy < runAwayDistance)
+			FleeFromTarget (enemyPosition);
 	}
 
-	void Update () 
-	{
-		Vector3 desination = targetGO.transform.position;
-		if (fleeFromTarget){
-			desination = PositionToFleeTowards(targetGO.transform.position);
-		}
-
-		HeadForDestintation(desination);
+	private void FleeFromTarget(Vector3 enemyPosition){
+		Vector3 fleeToPosition = Vector3.Normalize(transform.position - enemyPosition) * runAwayDistance;
+		HeadForDestintation(fleeToPosition);
 	}
-
-	private void HeadForDestintation (Vector3 destinationPosition)
-	{
+	
+	private void HeadForDestintation (Vector3 destinationPosition){
 		navMeshAgent.SetDestination (destinationPosition);
 	}
-	
-	private Vector3 PositionToFleeTowards(Vector3 targetPosition)
-	{
-		transform.rotation = Quaternion.LookRotation(transform.position - targetPosition);
-		Vector3 runToPosition = targetPosition + (transform.forward * runAwayDistance);
-		return runToPosition;
-		/*
-		 * code to ensure our run to position is a valid part of the NavMesh
-		NavMeshHit hit;
-		NavMesh.SamplePosition(runTo, out hit, 5, 1 << NavMesh.GetAreaFromName("Default"));
-		return hit.position;
-		*/
-	}
-
 }

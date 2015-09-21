e559ae258af3b7770ec147f9ed52869adfc698fd
dr-matt-smith <dr_matt_smith@me.com>
2015-09-21 21:34:21 +0100

    chapter 3 code commented

--- 1362_03_Animate2D/1362_03_Animate2D_codes/1362_03_01_beetleFlipLeftRight/Scripts/BugFlip.cs
+++ 1362_03_Animate2D/1362_03_Animate2D_codes/1362_03_01_beetleFlipLeftRight/Scripts/BugFlip.cs
@@ -1,19 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BugFlip : MonoBehaviour {
+/*
+ * simple class, to flip sprite left / right based on arrow keys
+ */
+public class BugFlip : MonoBehaviour
+{
+	// flag - are we facing right?
+	// (or if false, then we are facing left...)
 	private bool facingRight = true;
-	
-	void Update() {
-		if (Input.GetKeyDown(KeyCode.LeftArrow) && facingRight) Flip ();
-		if (Input.GetKeyDown(KeyCode.RightArrow) && !facingRight) Flip ();
+
+	//----------------------------------
+	void Update()
+	{
+		// flip if we are facing LEFT and press LEFT arrow key
+		if (Input.GetKeyDown(KeyCode.LeftArrow) && facingRight)
+			Flip ();
+
+		// flip if we are facing LEFT (!right) and press RIGHT arrow key
+		if (Input.GetKeyDown(KeyCode.RightArrow) && !facingRight)
+			Flip ();
 	}
 	
-	void Flip (){
+	//----------------------------------
+	void Flip ()
+	{
 		// Switch the way the player is labelled as facing.
 		facingRight = !facingRight;
 		
 		// Multiply the player's x local scale by -1.
+		// this results in left/right mirrow image swap
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;

7fec411070d497caba6d77b7e6c8d7b1e9f723d6
dr-matt-smith <dr_matt_smith@me.com>
2015-09-24 13:17:04 +0100

    Ch 11 - commented game manager and new image

--- 1362_11_Optimize/1362_11_Optimize_codes/1362_11_05_state_pattern/Scripts/GameManager.cs
+++ 1362_11_Optimize/1362_11_Optimize_codes/1362_11_05_state_pattern/Scripts/GameManager.cs
@@ -2,60 +2,89 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+/*
+ * class to setup and manage changes between game states
+ * and ensure enter / update / exit methods called for current state object
+ */
+public class GameManager : MonoBehaviour 
+{
+	// UI elements for onscreen messags, and buttons to change states
 	public Text textStateMessages;
 	public Button buttonWinGame;
 	public Button buttonLoseGame;
 
+	// variables that will reference objects for each state we might want our game to visit
 	public StateGamePlaying stateGamePlaying{get; set;}
 	public StateGameWon stateGameWon{get; set;}
 	public StateGameLost stateGameLost{get; set;}
-	
+
+	// reference to the object representing the current state of the game
 	private GameState currentState;
 
-	private void Awake () {
+	// create 3 state objects - for playing / won/ lost
+	private void Awake () 
+	{
 		stateGamePlaying = new StateGamePlaying(this);
 		stateGameWon = new StateGameWon(this);
 		stateGameLost = new StateGameLost(this);
 	}
-	
-	private void Start () {
+
+	// move the game into state 'stateGamePlaying'
+	private void Start () 
+	{
 		NewGameState( stateGamePlaying );
 	}
 
-	private void Update () {
-		if (currentState != null)
-			currentState.StateUpdate();
+	// if there is a currentState, then call its StateUpdate() method
+	private void Update () 
+	{
+		if (currentState != null) {
+			currentState.StateUpdate ();
+		}
 	}
-	
+
+	// actions to move game into a new state
 	public void NewGameState(GameState newState)
 	{
-		if( null != currentState)
-			currentState.OnMyStateExit();
-		
+		// if there is a current state, then call its OnMyStateExit() method
+		if (null != currentState) {
+			currentState.OnMyStateExit ();
+		}
+
+		// store reference to the new state object in variable 'currentState'
 		currentState = newState;
+
+		// call the OnMyStateEntered() method for this new state object
 		currentState.OnMyStateEntered();
 	}
 
-	public void DisplayStateEnteredMessage(string stateEnteredMessage){
+	// update the onscreen UI Text to show the message about the state just entered
+	public void DisplayStateEnteredMessage(string stateEnteredMessage)
+	{
 		textStateMessages.text = stateEnteredMessage;
 	}
-	
-	public void BUTTON_CLICK_ACTION_WIN_GAME(){
+
+	// move the game into the WIN GAME state
+	public void BUTTON_CLICK_ACTION_WIN_GAME()
+	{
 		if( null != currentState){
 			currentState.OnButtonClick(GameState.ButtonType.ButtonWinGame);
 			DestroyButtons();
 		}
 	}
-	
-	public void BUTTON_CLICK_ACTION_LOSE_GAME(){
+
+	// move the game into the LOSE GAME state
+	public void BUTTON_CLICK_ACTION_LOSE_GAME()
+	{
 		if( null != currentState){
 			currentState.OnButtonClick(GameState.ButtonType.ButtonLoseGame);
 			DestroyButtons();
 		}
 	}
 
-	private void DestroyButtons(){
+	// remove the 2 UI buttons from the screen
+	private void DestroyButtons()
+	{
 		Destroy (buttonWinGame.gameObject);
 		Destroy (buttonLoseGame.gameObject);
 	}
